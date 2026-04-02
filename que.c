233d05b3a51d9165905efe77aec79e23dc069f89
jihoo <jihoop0620@gmail.com>
2026-04-02 17:40:34 +0900

    rotate

--- que.c
+++ que.c
@@ -6,7 +6,20 @@ void print_arr(int *arr,int size) {
     }
     printf("\n");
 }
+int *rotate(int * arr,int size,int rotate) {
+    for (int j =0;j<rotate;j++) {
+        int temp = arr[size-1];
+        for (int i = size-1;i>0;i--) {
+            arr[i] = arr[i-1];
+        }
+        arr[1] = arr[0];
+        arr[0] = temp;
+    }
+    return arr;
+}
 int main() {
-    int arr[5] = {1,2,3,4,5};
-    print_arr(arr,5);
+    int arr[6] = {1,2,3,4,5,6};
+    print_arr(arr,6);
+    int * newarr = rotate(arr,6,6);
+    print_arr(newarr,6);
 }
